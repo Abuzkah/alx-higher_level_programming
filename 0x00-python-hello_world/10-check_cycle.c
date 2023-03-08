125e78c60072678218b2d30cbd0ad6e82d60ef94
Abuzkah <110993144+Abuzkah@users.noreply.github.com>
2023-03-08 17:50:10 -0500

    error fixed

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,31 +1,26 @@
 #include "lists.h"
 
 /**
- * check_cycle - check for loop in LL
- * @list: head of linked list
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
  *
- * Description - check for loops in LL
- * Return: 1 if cycled, 0 if not
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
 	if (!list)
-	{
 		return (0);
-	}
-	slow = list;
-	fast = list->next;
-	while (fast && slow && fast->next)
+
+	while (slow && fast && fast->next)
 	{
-		if (slow == fast)
-		{
-			return (1);
-		}
 		slow = slow->next;
 		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
 	}
+
 	return (0);
 }
